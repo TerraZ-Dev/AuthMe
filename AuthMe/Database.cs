fa30d1f33c61213e98f5da450bbf0e203fa24ec9
AkjaHAsLk1IALk0MasH <46046453+AgaSpace@users.noreply.github.com>
2022-05-28 21:28:01 +0700

    Добавьте файлы проекта.

---
+++ AuthMe/Database.cs
@@ -0,0 +1,81 @@
+﻿using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace AuthMe
+{
+    public class Manager
+    {
+        public static IDbConnection DB;
+        internal static void Initailize()
+        {
+            IQueryBuilder builder = null;
+            switch (TShock.Config.Settings.StorageType)
+            {
+                default:
+                    return;
+
+                case "mysql":
+                    var hostport = TShock.Config.Settings.MySqlHost.Split(':');
+                    DB = new MySqlConnection();
+                    DB.ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                        hostport[0],
+                        hostport.Length > 1 ? hostport[1] : "3306",
+                        TShock.Config.Settings.MySqlDbName,
+                        TShock.Config.Settings.MySqlUsername,
+                        TShock.Config.Settings.MySqlPassword);
+                    builder = new MysqlQueryCreator();
+                    break;
+                case "sqlite":
+                    DB = new SqliteConnection(string.Format("uri=file://tshock//{0},Version=3", "AuthMe.sqlite"));
+                    builder = new SqliteQueryCreator();
+                    break;
+            }
+
+            SqlTable table = new SqlTable("AuthMe",
+                new SqlColumn("From", MySqlDbType.Text),
+                new SqlColumn("To", MySqlDbType.Text));
+
+            new SqlTableCreator(DB, builder).EnsureTableStructure(table);
+        }
+
+        public static string GetFirstAccount(string toAccount)
+        {
+            using (var reader = DB.QueryReader("SELECT * FROM AuthMe WHERE To=@0", toAccount))
+            {
+                if (reader.Read())
+                {
+                    return reader.Get<string>("From");
+                }
+            }
+            return "";
+        }
+        public static string GetSecondAccount(string fromAccount)
+        {
+            using (var reader = DB.QueryReader("SELECT * FROM AuthMe WHERE From=@0", fromAccount))
+            {
+                if (reader.Read())
+                {
+                    return reader.Get<string>("To");
+                }
+            }
+            return "";
+        }
+
+        public static void Add(string from, string to)
+        {
+            DB.Query("INSERT INTO AuthMe VALUES(@0, @1)", from, to);
+        }
+        public static void Remove(string from)
+        {
+            DB.Query("DELETE FROM AuthMe WHERE From=@0", from);
+        }
+    }
+}
---
+++ AuthMe/Plugin.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.DB;
+using TShockAPI.Hooks;
+
+namespace AuthMe
+{
+    [ApiVersion(2, 1)]
+    public class AuthMePlugin : TerrariaPlugin
+    {
+        public override string Author => "Zoom L1";
+        public override string Name => "AuthMe";
+
+        public AuthMePlugin(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            Manager.Initailize();
+            
+            Commands.ChatCommands.Add(new Command("auth.me", AuthMeCommand, "authme", "auth"));
+
+            PlayerHooks.PlayerPostLogin += OnLogin;
+        }
+
+        public void OnLogin(PlayerPostLoginEventArgs args)
+        {
+            string to = Manager.GetSecondAccount(args.Player.Account.Name);
+            if (to != "")
+                LoginPlayer(args.Player, to);
+        }
+
+        public void AuthMeCommand(CommandArgs args)
+        {
+            if (args.Parameters.Count < 2)
+            {
+                args.Player.SendErrorMessage("Invalid command. Try /authme <account> <password>");
+                return;
+            }
+
+            string strAccount = args.Parameters[0];
+            string strPassword = args.Parameters[1];
+
+            var account = TShock.UserAccounts.GetUserAccountByName(strAccount);
+            if (account == null)
+            {
+                args.Player.SendErrorMessage("Wrong account.");
+                return;
+            }
+            if (!account.VerifyPassword(strPassword))
+            {
+                args.Player.SendErrorMessage("Wrong password.");
+                return;
+            }
+
+            Manager.Add(args.Player.Account.Name, strAccount);
+            args.Player.SendSuccessMessage("You confirmed your account, try '/logout' '/login'");
+        }
+
+        public static void LoginPlayer(TSPlayer player, string Account, bool msg = false)
+        {
+            UserAccount userAccountByName = TShock.UserAccounts.GetUserAccountByName(Account);
+            player.DataWhenJoined = new PlayerData(player);
+            player.DataWhenJoined.CopyCharacter(player);
+            player.PlayerData = new PlayerData(player);
+            player.PlayerData.CopyCharacter(player);
+            if (userAccountByName != null)
+            {
+                player.PlayerData = TShock.CharacterDB.GetPlayerData(player, userAccountByName.ID);
+                Group groupByName = TShock.Groups.GetGroupByName(userAccountByName.Group);
+                player.Group = groupByName;
+                player.tempGroup = null;
+                player.Account = userAccountByName;
+                player.IsLoggedIn = true;
+                player.IsDisabledForSSC = false;
+                if (Main.ServerSideCharacter)
+                {
+                    if (player.HasPermission(Permissions.bypassssc))
+                    {
+                        player.PlayerData.CopyCharacter(player);
+                        TShock.CharacterDB.InsertPlayerData(player, false);
+                    }
+                    player.PlayerData.RestoreCharacter(player);
+                }
+                player.LoginFailsBySsi = false;
+                if (player.HasPermission(Permissions.ignorestackhackdetection))
+                {
+                    player.IsDisabledForStackDetection = false;
+                }
+                if (player.HasPermission(Permissions.usebanneditem))
+                {
+                    player.IsDisabledForBannedWearable = false;
+                }
+
+                if (msg)
+                    player.SendSuccessMessage("Authenticated as " + userAccountByName.Name + " successfully.");
+
+                TShock.Log.ConsoleInfo(player.Name + " authenticated successfully as user " + player.Name + ".");
+                PlayerHooks.OnPlayerPostLogin(player);
+            }
+        }
+    }
+}
